c99f896e554df57982ebee6bcb9cc963190e5f5c
returnt <returnt.ru@gmail.com>
2016-01-04 14:18:52 +0200

    push 2_4

--- WebApplication_1/Checkout.aspx.cs
+++ WebApplication_1/Checkout.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace WebApplication_1
 {
-    public partial class Checkout : System.Web.UI.Page
+    public partial class Checkout : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
